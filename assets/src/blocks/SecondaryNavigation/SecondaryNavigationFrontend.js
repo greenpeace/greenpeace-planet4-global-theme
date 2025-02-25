b3d15c14517281e1558098aea934e49221b4e684
Osong Agberndifor <osongagberndifor@gmail.com>
2025-02-25 02:16:44 +0100

    PLANET-6530 Added more features
    - Added code to update url and active link on scroll

--- assets/src/blocks/SecondaryNavigation/SecondaryNavigationFrontend.js
+++ assets/src/blocks/SecondaryNavigation/SecondaryNavigationFrontend.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from '@wordpress/element';
+import {useState, useEffect, useRef} from '@wordpress/element';
 import {getHeadingsFromDom} from '../TableOfContents/getHeadingsFromDom';
 import {initializeJustifyContentAdjustment} from './adjustNavWidth';
 
@@ -26,12 +26,64 @@ const makeSecondaryNavigationStickyonScroll = () => {
 
 export const SecondaryNavigationFrontend = ({levels}) => {
   const [activeLink, setActiveLink] = useState('');
+  const isClicking = useRef(false);
+  const hasLoaded = useRef(false);
+
+
   const headings = getHeadingsFromDom(levels);
 
   useEffect(() => {
     makeSecondaryNavigationStickyonScroll();
     initializeJustifyContentAdjustment();
-  }, [headings]);
+  }, []);
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    const observerCallback = entries => {
+      if (isClicking.current) {return;}
+
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          if (hasLoaded.current) {
+            setActiveLink(entry.target.id);
+            window.history.replaceState(null, null, `#${entry.target.id}`);
+          }
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    headings.forEach(({anchor}) => {
+      const link = document.getElementById(anchor);
+      if (link) {observer.observe(link);}
+    });
+
+    setTimeout(() => {
+      hasLoaded.current = true;
+    }, 500);
+
+    return () => {
+      headings.forEach(({anchor}) => {
+        const link = document.getElementById(anchor);
+        if (link) {observer.unobserve(link);}
+      });
+    };
+  }, []);
+
+  const handleClick = id => {
+    isClicking.current = true;
+    setActiveLink(id);
+
+    setTimeout(() => {
+      isClicking.current = false;
+    }, 500);
+  };
 
   return (
     <section className="block secondary-navigation-block">
@@ -44,7 +96,8 @@ export const SecondaryNavigationFrontend = ({levels}) => {
               <a
                 className={`secondary-navigation-link ${activeLink === anchor ? 'active': ''}`}
                 href={`#${anchor}`}
-                onClick={() => setActiveLink(anchor)}
+                data-target={anchor}
+                onClick={() => handleClick(anchor)}
               >
                 {content}
               </a>
